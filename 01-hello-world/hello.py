5a6aeebeb0fc75de18ca33ac2fdfbdfe61318e0c
Nouman72884 <noumansatti7@gmail.com>
2020-07-01 23:03:40 +0500

    demo files

--- 01-hello-world/hello.py
+++ 01-hello-world/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Hello, Nouman!</h1>"
 
 @app.route('/user/<name>')
 def user(name):
@@ -11,4 +11,3 @@ def user(name):
 
 if __name__ == '__main__':
     app.run(debug=True)
-
